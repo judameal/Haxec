bcfe798cfe6093a7d11616a5bdcfaac0ab8b8498
judameal <judameal47@gmail.com>
2026-04-14 19:14:01 -0500

    Version 1.7

---
+++ api/jugadores.js
@@ -0,0 +1,33 @@
+import clientPromise from "../lib/mongodb.js";
+
+export default async function handler(req, res) {
+  const client = await clientPromise;
+  const db = client.db("haxball");
+  const jugadores = db.collection("jugadores");
+
+  if (req.method === "GET") {
+    const data = await jugadores.find().toArray();
+    return res.status(200).json(data);
+  }
+
+  if (req.method === "POST") {
+    let body = "";
+
+    await new Promise((resolve) => {
+      req.on("data", chunk => body += chunk);
+      req.on("end", resolve);
+    });
+
+    const { dorsal, nombre, equipo } = JSON.parse(body);
+
+    if (!dorsal || !nombre || !equipo) {
+      return res.status(400).json({ message: "Faltan datos" });
+    }
+
+    await jugadores.insertOne({ dorsal, nombre, equipo });
+
+    return res.status(200).json({ message: "Jugador creado" });
+  }
+
+  res.status(405).end();
+}
